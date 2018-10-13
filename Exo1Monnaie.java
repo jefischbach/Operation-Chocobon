0a19c118755b280d26cda8c7754c0523ff592d74
Jean-Gabriel Fischbach <jean-gabriel.fischbach@hotmail.fr>
2018-10-13 18:07:22 +0200

    Update Exo1Monnaie.java
    import non utilisé

--- Exo1Monnaie.java
+++ Exo1Monnaie.java
@@ -5,7 +5,6 @@ import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
-import org.chocosolver.solver.variables.Variable;
 
 /**
  * @author Jean-Gabriel Fischbach
