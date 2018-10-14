82cf9a2e1b805c2029a56479b1a1e8f0273ca769
g-gaudin <34863251+g-gaudin@users.noreply.github.com>
2018-10-14 20:04:55 +0200

    Add files via upload

--- Exo3Sel.java
+++ Exo3Sel.java
@@ -2,7 +2,7 @@ package exo3sel;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.solver.variables.BoolVar;
 
 /**
  * @author Guillaume Gaudin
@@ -13,39 +13,58 @@ public class Exo3Sel {
         
         Model model = new Model();
         
-        //On modélise le choix de condiment par un tableau de valeur : nom[0] pour le sel nom[1] pour la moutarde
+        //On modélise le v de condiment par un tableau de valeur : nom[0] pour le sel nom[1] pour la moutarde
         
         //Variable Barnabé
-        IntVar[] choixB = model.intVarArray("choixB", 2,0,1);
+        BoolVar[] vB = model.boolVarArray("vB", 2);
         //Variable Casimir
-        IntVar[] choixC = model.intVarArray("choixC", 2,0,1);
+        BoolVar[] vC = model.boolVarArray("vC", 2);
         //Variable Desiré
-        IntVar[] choixD = model.intVarArray("choixD", 2,0,1);
+        BoolVar[] vD = model.boolVarArray("vD", 2);
         //Variable Ludovic
-        IntVar[] choixL = model.intVarArray("choixL", 2,0,1);
+        BoolVar[] vL = model.boolVarArray("vL", 2);
         //Variable Martial
-        IntVar[] choixM = model.intVarArray("choixM", 2,0,1);
+        BoolVar[] vM = model.boolVarArray("vM", 2);
         
         //Barnabé prend du sel si et seulement si Casimir ne prend que du sel ou que de la moutarde.
-        model.ifThen( model.or( model.and( model.arithm( choixC[0], "=", 1), model.arithm( choixC[1], "=", 0)), model.and( model.arithm( choixC[0], "=", 0), model.arithm( choixC[1], "=", 1))), model.arithm( choixB[0], "=", 1));
+        BoolVar C1 = model.or(model.and(vC[0], vC[1].not()).reify(), model.and(vC[0].not(), vC[1]).reify()).reify();
+        model.or(model.and(C1, vB[0]).reify(), model.and(C1.not(), vB[0].not()).reify()).post();
+        
         //Il prend de la moutarde si et seulement si, ou bien Désiré ne prend ni sel ni moutarde, ou bien Martial prend les deux.
-        model.ifThen( model.or( model.and( model.arithm( choixD[0], "=", 0), model.arithm( choixD[1], "=", 0)), model.and( model.arithm( choixM[0], "=", 1), model.arithm( choixM[1], "=", 1))), model.arithm( choixB[1], "=", 1));
+        BoolVar C2 = model.or(model.and(vD[0].not(), vD[1].not()).reify(), model.and(vM[0], vM[1]).reify()).reify();
+        model.or(model.and(C2, vB[1]).reify(), model.and(C2.not(), vB[1].not()).reify()).post();
+        
         //Casimir prend du sel si et seulement si, ou bien Barnabé ne prend qu'un des deux condiments, ou bien Martial n'en prend aucun.
-        model.ifThen( model.or( model.or( model.and( model.arithm( choixB[0] , "=", 1),model.arithm( choixB[1], "=", 0)), model.and( model.arithm( choixB[0], "=", 0), model.arithm( choixB[1],"=",1))), model.and(model.arithm(choixM[0],"=",0),model.arithm(choixM[1],"=",0))), model.arithm(choixC[0],"=",1));
+        BoolVar C3 = model.or( model.or(model.and(vB[0], vB[1].not()).reify(),model.and(vB[0].not(), vB[1]).reify()).reify(), model.and(vM[0].not(), vM[1].not()).reify()).reify();
+        model.or(model.and(C3, vC[0]).reify(), model.and(C3.not(), vC[0].not()).reify()).post();
+        
         //Il prend de la moutarde si et seulement si Désiré ou Ludovic prennent les deux condiments.
-        model.ifThen( model.or( model.and( model.arithm( choixD[0], "=", 1), model.arithm( choixD[1], "=", 1)), model.and( model.arithm( choixL[0], "=", 1), model.arithm( choixL[1], "=", 1))), model.arithm( choixC[1], "=", 1));
+        BoolVar C4 = model.or(model.and(vD[1], vD[0]).reify(), model.and(vL[1], vL[0]).reify()).reify();
+        model.or(model.and(C4, vC[1]).reify(), model.and(C4.not(), vC[1].not()).reify()).post();
+        
         //Désiré prend du sel si et seulement si ou bien Barnabé ne prend aucun condiment, ou bien Casimir prend les deux.
-        model.ifThen( model.or( model.and( model.arithm( choixB[0], "=", 0), model.arithm( choixB[1], "=", 0)), model.and( model.arithm( choixC[0], "=", 1), model.arithm( choixC[1], "=", 1))), model.arithm( choixD[0], "=", 1));
+        BoolVar C5 = model.or(model.and(vB[0].not(), vB[1].not()).reify(), model.and(vC[0], vC[1]).reify()).reify();
+        model.or(model.and(C5, vD[0]).reify(), model.and(C5.not(), vD[0].not()).reify()).post();
+        
         //Il prend de la moutarde si et seulement si Ludovic ou Martial ne prennent ni sel ni moutarde.
-        model.ifThen( model.or( model.and( model.arithm( choixL[0], "=", 0), model.arithm( choixL[1], "=", 0)), model.and( model.arithm( choixM[0], "=", 0), model.arithm( choixM[1], "=", 0))), model.arithm( choixD[1], "=", 1));
+         BoolVar C6 = model.or(model.and(vL[1].not(), vL[0].not()).reify(), model.and(vM[1].not(), vM[0].not()).reify()).reify();
+        model.or(model.and(C6, vD[1]).reify(), model.and(C6.not(), vD[1].not()).reify()).post();
+        
         //Ludovic prend du sel si et seulement si Barnabé ou Désiré ne prennent ni sel ni moutarde.
-        model.ifThen( model.or( model.and( model.arithm( choixB[0], "=", 0), model.arithm( choixB[1], "=", 0)), model.and( model.arithm( choixD[0], "=", 0), model.arithm( choixD[1], "=", 0))), model.arithm( choixL[0], "=", 1));
+        BoolVar C7 = model.or(model.and(vB[1].not(), vB[0].not()).reify(), model.and(vD[1].not(), vD[0].not()).reify()).reify();
+        model.or(model.and(C7, vL[0]).reify(), model.and(C7.not(), vL[0].not()).reify()).post();
+        
         //Il prend de la moutarde si et seulement si Casimir ou Martial ne prennent ni sel, ni moutarde.
-        model.ifThen( model.or( model.and( model.arithm( choixC[0], "=", 0), model.arithm( choixC[1], "=", 0)), model.and( model.arithm( choixM[0], "=", 0), model.arithm( choixM[1], "=", 0))), model.arithm( choixL[1], "=", 1));
+        BoolVar C8 = model.or(model.and(vC[0].not(), vC[1].not()).reify(), model.and(vM[0].not(), vM[1].not()).reify()).reify();
+        model.or(model.and(C8, vL[1]).reify(), model.and(C8.not(), vL[1].not()).reify()).post();
+        
         //Martial prend du sel si et seulement si Barnabé ou Ludovic prennent des deux condiments. 
-        model.ifThen( model.or( model.and( model.arithm( choixB[0], "=", 1), model.arithm( choixB[1], "=", 1)), model.and( model.arithm( choixL[0], "=", 1), model.arithm( choixL[1], "=", 1))), model.arithm( choixM[0], "=", 1));
+        BoolVar C9 = model.or(model.and(vB[1], vB[0]).reify(), model.and(vL[0], vL[1]).reify()).reify();
+        model.or(model.and(C9, vM[0]).reify(), model.and(C9.not(), vM[0].not()).reify()).post();
+        
         //Il prend de la moutarde si et seulement si Casimir ou Désiré ne prennent qu'un seul condiment.
-        model.ifThen( model.or( model.or( model.and( model.arithm( choixC[0], "=", 1), model.arithm( choixC[1], "=", 0)), model.and( model.arithm( choixC[0], "=", 0), model.arithm( choixC[1], "=", 1))), model.or(model.and( model.arithm( choixD[0], "=", 1), model.arithm( choixD[1], "=", 0)), model.and( model.arithm( choixD[0], "=", 0), model.arithm( choixD[1], "=", 1)))), model.arithm( choixM[1], "=", 1));
+        BoolVar C10 = model.or(model.and(model.or(vC[1],vC[0]).reify(), model.and(vC[1],vC[0]).reify().not()), model.and(model.or(vD[1], vD[0]).reify(), model.and(vD[1], vD[0]).reify().not())).reify();
+        model.or(model.and(C10, vM[1]).reify(), model.and(C10.not(), vM[1].not()).reify()).post();
         
         Solution solution = model.getSolver().findSolution();
         if(solution != null){
